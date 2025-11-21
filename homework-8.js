b8b94cad754a1ca63dfafb27e2df875f1cf6278c
Dev12w <absa0505@gmail.com>
2025-11-21 23:01:14 +0300

    completed tasks number eight

--- homework-8.js
+++ homework-8.js
@@ -1,66 +1,58 @@
-/*Домашнее задание №8:
-
-Опираясь на данную лекцию (https://t.me/c/2614739958/5341/9306):
-
-1. Как и в прошлых задания - создаем отдельный файл для homework-8 и подключаем его в HTML с атрибутом type = module (что бы работали импорты)
-
-2. Создаем файл js, где будет хранится массив объектов, которые представляют собой продуктовые карточки из вёрстки. 
-   Вам нужно описать полностью объект и продублировать его для всех карточек и после импортировать в homework-8 для дальнейшей работы с ним.
-
-3. По аналогии из лекции — создать и реализовать шаблон для продуктовых карточек.
-
-4. Используя метод .reduce(), получить строку, которая состоит из названий продуктовых карточек, разделенных точкой с запятой
-
-5. Используя метод .reduce(), получить массив объектов, где ключем является название продукта, а значением - его описание
-
-6*. Реализовать функцию, которая при старте нашей страницы выводит сообщение с текстом, 
-    мол "Сколько карточек отобразить? От 1 до 5" и в зависимости от результата - будет выводить это количество.
-    Должна быть защита от введенных других значений (имеется ввиду проверка if).
-
-P.S - код из задания №3 переместить в функцию, не нужно его дублировать
-
-P.S. - визуально, карточки у вас не должны поменяться вообще.
-*/
+// 1. Cоздаем отдельный файл для homework-8 module
+// 2. Создан файл product-cards c с массивом карточек
 
 import { cards } from "./product-cards.js";
 
 const productContainer = document.querySelector(".product-container")
 const cardContainer = document.querySelector(".product-template-container")
+const textContentError = document.querySelector(".textContentError")
+const input = document.querySelector(".product-cards-input")
 
-cards.forEach(product => {
-  const productCard = cardContainer.content.cloneNode(true);
-  productCard.querySelector(".product-card-image").src = product.image
-  productCard.querySelector(".product-category").textContent = product.category
-  productCard.querySelector(".product-name").textContent = product.name
-  productCard.querySelector(".product-description").textContent = product.description
-  productCard.querySelector(".product-compound-text").textContent = product.compoundText
+// 6*. Функция которая принимает в поле ввода от 1 до 5 и отображает карточки, с валидацией.
 
-  productCard.querySelector(".product-compound-list").innerHTML = product.compoundList
-    .map(compound => `<li class="product-compound">${compound}</li>`)
-    .join("");
+input.addEventListener('change', () => {
+  const value = input.value
+  productContainer.innerHTML = '';
+  textContentError.textContent = ''
 
-  productCard.querySelector(".product-price-lable").textContent = product.lable
-  productCard.querySelector(".product-price").textContent = product.price + " \u20BD"
-  productContainer.appendChild(productCard)
+  if (isNaN(value)) {
+    textContentError.textContent = 'Введите число!'
+    input.value = '';
+  }
+  if (value > 5) {
+    textContentError.textContent = 'Введите число от 1 до 5'
+    input.value = '';
+  }
+
+  const selectedCards = cards.slice(0, value)
+
+//3. Шаблон для продуктовых карточек.
+
+  selectedCards.forEach(product => {
+    const productCard = cardContainer.content.cloneNode(true);
+    productCard.querySelector(".product-card-image").src = product.image
+    productCard.querySelector(".product-category").textContent = product.category
+    productCard.querySelector(".product-name").textContent = product.name
+    productCard.querySelector(".product-description").textContent = product.description
+    productCard.querySelector(".product-compound-text").textContent = product.compoundText
+
+    productCard.querySelector(".product-compound-list").innerHTML = product.compoundList
+      .map(compound => `<li class="product-compound">${compound}</li>`)
+      .join("");
+
+    productCard.querySelector(".product-price-lable").textContent = product.lable
+    productCard.querySelector(".product-price").textContent = product.price + " \u20BD"
+    productContainer.appendChild(productCard)
+  })
+  input.value = ''
 })
 
 
 // 4. Используя метод .reduce(), получить строку, которая состоит из названий продуктовых карточек, разделенных точкой с запятой
 
-const productNamesList  = cards.reduce((acc, card) => acc === "" ? card.name : acc + "; " + card.name,"");
+const productNamesList = cards.reduce((acc, card) => acc === "" ? card.name : acc + "; " + card.name, "");
 
 
 // 5. Используя метод .reduce(), получить массив объектов, где ключем является название продукта, а значением - его описание
 
-const productsDescriptions = cards.reduce((acc, card) => [...acc, {[card.name]: card.description}],[])
-
-
-// 6*. Реализовать функцию, которая при старте нашей страницы выводит сообщение с текстом, 
-//     мол "Сколько карточек отобразить? От 1 до 5" и в зависимости от результата - будет выводить это количество.
-//     Должна быть защита от введенных других значений (имеется ввиду проверка if)
-
-const messageQuestion = () => {
-  prompt()
-}
-
-console.log(messageQuestion)
+const productsDescriptions = cards.reduce((acc, card) => [...acc, { [card.name]: card.description }], [])
