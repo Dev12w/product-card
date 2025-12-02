f9c3915f2822153a52e63244f0b13cc4b0c2704a
Dev12w <absa0505@gmail.com>
2025-12-03 01:00:04 +0300

    fix error

--- homework-9.js
+++ homework-9.js
@@ -2,12 +2,9 @@
 
 const buttonSubscribe = document.querySelector('.footer-email-btn-subscribe')
 const footerEmail = document.querySelector('.footer-email-form-container')
-
 const emailValidationRegexp = /^[^\s]+@gmail\.com$/
 
-
 buttonSubscribe.addEventListener('click', (e) => {
-  e.preventDefault()
 
   const formData = new FormData(footerEmail)
   const data = Object.fromEntries(formData)
@@ -20,146 +17,121 @@ buttonSubscribe.addEventListener('click', (e) => {
   console.log(data)
 })
 
-
-
 // 5.Модальное окно формы регестрации. 
 // поля: имя, фамилия, дата рождения, логин, пароль, повторение пароля. 
 
-const buttonRegistration = document.querySelector('.btn-registration')
-const formRegTemplate = document.querySelector('.form-registration-content')
-
 // 6. Сохраняем этот объект в переменную для дальнейшего использования.
+const modalReg = document.querySelector('.modal-reg')
+const btnRegistration = document.querySelector('.btn-registration')
 let currentUser = undefined;
 
+btnRegistration.addEventListener('click', () => {
+  modalReg.classList.add('modal-showed')
+})
 
-buttonRegistration.addEventListener('click', () => {
+const modalRegCloseBtn = document.querySelector('.modal-reg-close-button')
 
-  const formClon = formRegTemplate.content.cloneNode(true)
+modalRegCloseBtn.addEventListener('click', () => {
+  modalReg.classList.remove('modal-showed')
+})
 
-  const formErrorEmailText = formClon.querySelector('.error-email')
-  const registrationForm = formClon.querySelector('.form')
-  const modalCloseBtn = formClon.querySelector('.modal-close-button')
-  const formRegBtn = formClon.querySelector('.enter-button')
-  const passwordRepeatErrorText = formClon.querySelector('.error-passwordrepeat')
-  const closeModal = formClon.querySelector('.modal')
+const registrationForm = document.querySelector('.form-reg')
+const formRegBtn = document.querySelector('.reg-enter-button')
 
+formRegBtn.addEventListener('click', () => {
+  const formInputs = registrationForm.querySelectorAll('input');
+  let hasError = false;
 
-  modalCloseBtn.addEventListener('click', () => {
-    closeModal.remove()
-  })
+  formInputs.forEach(el => {
+    el.classList.remove('error');
 
-
-  formRegBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-
-    const formInputs = registrationForm.querySelectorAll('input');
-    let hasError = false;
-
-    formInputs.forEach(el => {
-      el.classList.remove('error');
-
-      if (el.hasAttribute('required') && el.value.trim() === '') {
-        el.classList.add('error');
-        hasError = true;
-        return
-      }
-
-      if (el.type == "email") {
-        formErrorEmailText.textContent = '';
-        if (!emailValidationRegexp.test(el.value)) {
-          el.classList.add('error')
-          formErrorEmailText.textContent = 'Некоректный email'
-          hasError = true;
-        }
-      }
-    })
-
-    const formData = new FormData(registrationForm)
-    const data = Object.fromEntries(formData)
-
-
-    const passwordRepeat = document.querySelector('#form-input-passwordrepeat');
-    if (data.password !== data.passwordrepeat) {
-      passwordRepeat.classList.add('error')
-      passwordRepeatErrorText.textContent = 'Пароли не совпадают'
+    if (el.hasAttribute('required') && el.value.trim() === '') {
+      el.classList.add('error');
       hasError = true;
-    } else {
-      passwordRepeat.classList.remove('error')
-      passwordRepeatErrorText.textContent = ''
+      return
     }
+    const formErrorEmailText = document.querySelector('.error-email')
 
-    if (hasError) {
-      return;
+    if (el.type == "email") {
+      formErrorEmailText.textContent = '';
+      if (!emailValidationRegexp.test(el.value)) {
+        el.classList.add('error')
+        formErrorEmailText.textContent = 'Некоректный email'
+        hasError = true;
+      }
     }
-
-    currentUser = {
-      email: data.email,
-      password: data.password,
-      firstname: data.firstname,
-      surname: data.surname,
-      birthday: data.birthday,
-    };
-
-    currentUser.createdOn = new Date()
-
-    if (formInputs.value !== '') {
-      passwordRepeatErrorText.classList.add('successful-reg')
-      passwordRepeatErrorText.textContent = 'Успешная регестрация'
-      registrationForm.reset();
-      formInputs.forEach(input => input.disabled = true)
-      formRegBtn.disabled = true;
-    }
-
   })
-  document.body.appendChild(formClon)
+
+  const formData = new FormData(registrationForm)
+  const data = Object.fromEntries(formData)
+
+  const passwordRepeatErrorText = document.querySelector('.error-passwordrepeat')
+  const passwordRepeat = document.querySelector('#form-input-passwordrepeat');
+
+  if (data.password !== data.passwordrepeat) {
+    passwordRepeat.classList.add('error')
+    passwordRepeatErrorText.textContent = 'Пароли не совпадают'
+    hasError = true;
+  } else {
+    passwordRepeat.classList.remove('error')
+    passwordRepeatErrorText.textContent = ''
+  }
+
+  if (hasError) {
+    return;
+  }
+
+  currentUser = {
+    email: data.email,
+    password: data.password,
+    firstname: data.firstname,
+    surname: data.surname,
+    birthday: data.birthday,
+  };
+
+  currentUser.createdOn = new Date()
+
+  passwordRepeatErrorText.classList.add('successful-reg')
+  passwordRepeatErrorText.textContent = 'Успешная регистрация'
+  registrationForm.reset();
+  formInputs.forEach(input => input.disabled = true)
+  formRegBtn.disabled = true;
+})
+
+// 7. Кнопку "Аутентификация"
+// 8. Создается модальное окно  
+const loginButton = document.querySelector('.btn-login')
+const modalLogin = document.querySelector('.modal-login')
+
+loginButton.addEventListener('click', () => {
+  modalLogin.classList.add('modal-showed')
+})
+
+const modalLoginCloseBtn = document.querySelector('.modal-login-close-button')
+
+modalLoginCloseBtn.addEventListener('click', () => {
+  modalLogin.classList.remove('modal-showed')
 })
 
 
-// 7. Кнопку "Аутентификация"
+const loginEnterBtn = document.querySelector('.login-enter-button')
 
-const loginButton = document.querySelector('.btn-login')
-const loginTemplate = document.querySelector('.form-login-content')
+loginEnterBtn.addEventListener('click', () => {
+  const loginForm = document.querySelector('.form-login')
+  const errorText = document.querySelector('.login-error-text')
 
-// 8. Создается модальное окно  
+  const formData = new FormData(loginForm)
+  const data = Object.fromEntries(formData)
 
-loginButton.addEventListener('click', (e) => {
-  e.preventDefault()
+  errorText.textContent = ''
 
-  const formClon = loginTemplate.content.cloneNode(true)
+  if (!currentUser || currentUser.email !== data.email || currentUser.password !== data.password) {
+    errorText.textContent = 'Неправильный логин или пароль'
+    return
+  }
 
-  const loginForm = formClon.querySelector('.form')
-  const closeModal = formClon.querySelector('.modal')
-  const errorText = formClon.querySelector('.error-text')
-  const enterButton = formClon.querySelector('.enter-button')
-  const modalCloseBtn = formClon.querySelector('.modal-close-button')
+  // 10. Переменную "currentUser" добавлено дата и время входа.
 
-
-  modalCloseBtn.addEventListener('click', () => {
-    closeModal.remove()
-  })
-
-  enterButton.addEventListener('click', () => {
-
-    const formData = new FormData(loginForm)
-    const data = Object.fromEntries(formData)
-    errorText.textContent = ''
-
-    if (currentUser === undefined) {
-      errorText.textContent = 'Ползователь не найден'
-      return
-    }
-
-    if (currentUser.email !== data.email || currentUser.password !== currentUser.password) {
-      errorText.textContent = 'Неправильный логин или пароль'
-      return
-    }
-
-    // 10. Переменную "currentUser" добавлено дата и время входа.
-
-    currentUser.lastLogin = new Date()
-    console.log(currentUser)
-    closeModal.remove();
-  })
-
-  document.body.appendChild(formClon)
+  currentUser.lastLogin = new Date()
+  modalLogin.remove();
 })
