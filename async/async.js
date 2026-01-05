a7d17ba7f1c5a433ba2826cc61d2737730928985
Dev12w <absa0505@gmail.com>
2026-01-05 13:25:01 +0300

    fix

--- async/async.js
+++ async/async.js
@@ -1,4 +1,4 @@
-import { UserStore } from './User-store.js'
+import { UserStore } from './user-store.js'
 
 const userCards = document.querySelector('.user-cards');
 const userCardTemplate = document.querySelector('.user-card-template');
